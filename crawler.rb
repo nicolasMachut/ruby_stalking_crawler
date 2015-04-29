1b6ee13e546256ed974d74e0f1394ab4e5dccd53
nicolasMachut <machut.nicolas@live.fr>
2015-04-29 10:02:05 +0200

    add crawler class

--- crawler.rb
+++ crawler.rb
@@ -1,2 +1,24 @@
+require 'abstraction'
 class Crawler
+  abstract
+
+  def crawl
+    raise 'Override this method please !'
+  end
+
+  def start
+    crawl
+  end
+
+  def self.instantiate (type)
+    case type
+      when 'crawlerGoogle'
+        return CrawlerGoogle.new()
+      when 'crawlerTweeter'
+        return CrawlerTweeter.new()
+      when 'crawlerFaceBook'
+        return CrawlerFacebook.new()
+    end
+  end
+
 end
