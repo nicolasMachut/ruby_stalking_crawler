0a2c2bc0c118121174a00de87f4101f0e3519666
edouardsouan <souan.edouard0@gmail.com>
2015-05-12 12:36:02 +0200

    Fetching followers

--- Gemfile
+++ Gemfile
@@ -1,3 +1,5 @@
+source 'https://rubygems.org'
 
 gem 'oauth1', '~> 0.0.1'
 gem 'oauth'
+gem 'twitter'
--- crawler-twitter.rb
+++ crawler-twitter.rb
@@ -1,37 +1,24 @@
 require 'oauth'
-require 'net/http'
+require 'twitter'
 
 class CrawlerTwitter #< Crawler
-  @api_key = "Dsxw2Mcz6YUjkXn391hlDKNUZ"
-  @api_secret = "1xdi8rJlUpZnLXjZfxDBjMrUHVDrxTsoawgmuIvHZi8rJlPox9"
-  @token = "1076693654-uNeDfTiSqdNSfsDW3sebZAHJZS22VsBjhHbMbqp"
-  @token_secret = "PQ5BSo61nRQNCSuRnUpkco6W5xhk0ALdwJxodxNwWApjA"
+  #twitterID = 1076693654
 
   def crawl
-    res = oauth
-    puts res
+    api_key = "Dsxw2Mcz6YUjkXn391hlDKNUZ"
+    api_secret = "1xdi8rJlUpZnLXjZfxDBjMrUHVDrxTsoawgmuIvHZi8rJlPox9"
+    token = "1076693654-uNeDfTiSqdNSfsDW3sebZAHJZS22VsBjhHbMbqp"
+    token_secret = "PQ5BSo61nRQNCSuRnUpkco6W5xhk0ALdwJxodxNwWApjA"
+    client = Twitter::REST::Client.new do |config|
+      config.consumer_key        = api_key
+      config.consumer_secret     = api_secret
+      config.access_token        = token
+      config.access_token_secret = token_secret
+    end
+    response = client.followers("Edouard0Souan")
+    puts response.to_h
   end
-
-  def oauth
-    # Exchange your oauth_token and oauth_token_secret for an AccessToken instance.
-    def prepare_access_token(oauth_token, oauth_token_secret)
-      consumer = OAuth::Consumer.new(@api_key, @api_secret, { :site => "https://api.twitter.com", :scheme => :header })
-
-      # now create the access token object from passed values
-      token_hash = { :oauth_token => oauth_token, :oauth_token_secret => oauth_token_secret }
-      access_token = OAuth::AccessToken.from_hash(consumer, token_hash )
-
-      return access_token
-    end
-
-# Exchange our oauth_token and oauth_token secret for the AccessToken instance.
-    access_token = prepare_access_token(@token, @token_secret)
-
-# use the access token as an agent to get the home timeline
-    response = access_token.request(:get, "https://api.twitter.com/1.1/friends/list.json")
-    return response
-    end
 end
-twitter = CrawlerTwitter.new
 
+twitter = CrawlerTwitter.new
 twitter.crawl
